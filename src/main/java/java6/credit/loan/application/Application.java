ac1cd23780a3f145814f698db333bfdd96fc895b
swampus <dmitrijs.gavrilovs.swampus@gmail.com>
2020-07-04 08:21:11 +0100

    project skeleton

---
+++ src/main/java/java6/credit/loan/application/Application.java
@@ -0,0 +1,17 @@
+package java6.credit.loan.application;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    private static Logger LOG
+            = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        LOG.info("Application started");
+        SpringApplication.run(Application.class, args);
+    }
+}
