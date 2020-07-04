46d245de2a28ca7c7107f5bfd74c793babad6e77
swampus <dmitrijs.gavrilovs.swampus@gmail.com>
2020-07-04 11:59:47 +0100

    LoanApplication status / controller

--- src/main/java/java6/credit/loan/application/controller/LoanApplicationController.java
+++ src/main/java/java6/credit/loan/application/controller/LoanApplicationController.java
@@ -4,29 +4,37 @@ import java6.credit.loan.application.model.LoanApplication;
 import java6.credit.loan.application.model.LoanApplicationStatus;
 import java6.credit.loan.application.service.LoanApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@RestController("/api/rest/LoanApplication.svc")
 public class LoanApplicationController {
 
     @Autowired
     private LoanApplicationService loanApplicationService;
 
+    @PostMapping("/loanApplication")
     public void saveLoanApplication(LoanApplication loanApplication) {
         loanApplicationService.saveLoanApplication(loanApplication);
     }
 
-    public LoanApplication getLoanApplicationById(Long id) {
+    @GetMapping("/loanApplication/{id}")
+    public LoanApplication getLoanApplicationById(@PathVariable Long id) {
         return loanApplicationService.getLoanApplicationById(id);
     }
 
+    @GetMapping("/loanApplications")
     public List<LoanApplication> getLoanApplications() {
         return loanApplicationService.getAllLoanApplications();
     }
 
-    public void changeLoanApplicationStatus(Long id,
+
+    @PostMapping("/loanApplication/{id}/status")
+    public void changeLoanApplicationStatus(@PathVariable Long id,
                                             LoanApplicationStatus loanApplicationStatus) {
         loanApplicationService
                 .changeLoanApplicationStatus(id, loanApplicationStatus);
