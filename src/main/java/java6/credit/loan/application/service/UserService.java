a513aef41d390f853315ad78bb9e95f1ad06df6e
swampus <dmitrijs.gavrilovs.swampus@gmail.com>
2020-07-04 11:27:04 +0100

    more methods

--- src/main/java/java6/credit/loan/application/service/UserService.java
+++ src/main/java/java6/credit/loan/application/service/UserService.java
@@ -1,8 +1,34 @@
 package java6.credit.loan.application.service;
 
+import java6.credit.loan.application.exception.LoanApplicationNotFoundException;
+import java6.credit.loan.application.model.LoanApplication;
+import java6.credit.loan.application.model.User;
+import java6.credit.loan.application.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElseThrow(
+                () -> new LoanApplicationNotFoundException(
+                        "User with id: " + id + " Not found!"));
+    }
+
+    public List<User> getAllUsers() {
+        Iterable<User> iterable
+                = userRepository.findAll();
+        List<User> result
+                = new ArrayList<>();
+        iterable.forEach(result::add);
+        return result;
+    }
 
 }
